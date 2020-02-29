a103080e8fbaadd9d45025f47af51f1882a295a7
Nick Fada <nfada2010@gmail.com>
2020-02-29 13:31:05 -0500

    added style and talyout to tagPage

--- src/main/java/org/wcci/blog/Model/Tag.java
+++ src/main/java/org/wcci/blog/Model/Tag.java
@@ -6,10 +6,12 @@ public class Tag {
 
     private String name;
     private Post post;
+    private String title;
 
-    public Tag(String name, Post post) {
+    public Tag(String name, Post post,String title) {
         this.name = name;
         this.post = post;
+        this.title = title;
     }
 
     public Tag(){}
@@ -22,18 +24,23 @@ public class Tag {
         return post;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Tag tag = (Tag) o;
         return Objects.equals(name, tag.name) &&
-                Objects.equals(post, tag.post);
+                Objects.equals(post, tag.post) &&
+                Objects.equals(title, tag.title);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, post);
+        return Objects.hash(name, post, title);
     }
 
     @Override
@@ -41,6 +48,7 @@ public class Tag {
         return "Tag{" +
                 "name='" + name + '\'' +
                 ", post=" + post +
+                ", title='" + title + '\'' +
                 '}';
     }
 }
